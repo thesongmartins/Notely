722212c55b0a7adcdd26a75c408e52ec1e37bc72
thesongmartins <martinsjehoshaphat@gmail.com>
2025-09-21 17:35:24 +0100

    Updates

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
